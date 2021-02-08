35472c7b602fc4dc2ee1ef0693c6e5a0d68d7868
Punsara Bandara <78367312+punxara@users.noreply.github.com>
2021-02-08 09:02:09 +0530

    Frontend - React

---
+++ src/App.js
@@ -0,0 +1,27 @@
+
+import './App.css';
+
+function App() {
+  return (
+  <body>
+    <div>
+      <h1 className="App">YOMED!</h1>
+      <ul>
+      <li><a href="default.asp">Home</a></li>
+      <li><a href="news.asp">Order</a></li>
+      <li><a href="contact.asp">History</a></li>
+      <li><a href="about.asp">Weather</a></li>
+      </ul>
+      <p>
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+      Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure 
+      dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non 
+      proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+      </p>
+    </div>
+
+    </body>
+  );
+}
+
+export default App;
